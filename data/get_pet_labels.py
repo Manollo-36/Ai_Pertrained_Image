71ed561a18f26ad0a18102df91e2260456ecf498
Emmanuel Varsakis <Varsakise@gmail.com>
2025-02-07 10:39:34 +0000

    Added Implentation get dog breed from filename

--- data/get_pet_labels.py
+++ data/get_pet_labels.py
@@ -40,6 +40,42 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
+     # Creates list of files in directory
+    file_list = listdir(image_dir)
+    # Processes each of the files to create a dictionary where the key
+    # is the filename and the value is the picture label 
+ 
+    # Creat empty dictionary for the results
+    results_dictionary = dict()
+   
+    # Processes through each file in the directory, extracting only the words
+    # of the file that contain the pet image label
+    for idx in range(0, len(file_list), 1):
+       
+       # Skips file if starts with . (like .DS_Store of Mac OSX) because it 
+       # isn't an pet image file
+       if file_list[idx][0] != ".":
+           
+           # Creates temporary label variable to hold pet label name extracted 
+           pet_label = ""
+
+           # TODO: 2a. BELOW REPLACE pass with CODE that will process each 
+           #          filename in the in_files list to extract the dog breed 
+           #          name from the filename. Recall that each filename can be
+           #          accessed by in_files[idx]. Be certain to place the 
+           #          extracted dog breed name in the variable pet_label 
+           #          that's created as an empty string ABOVE
+           pet_label = file_list[idx].lower().strip().split("_")
+           print(pet_label) 
+           # If filename doesn't already exist in dictionary add it and it's
+           # pet label - otherwise print an error message because indicates 
+           # duplicate files (filenames)
+           if file_list[idx] not in results_dictionary:
+              results_dictionary[in_files[idx]] = [pet_label]
+              
+           else:
+               print("** Warning: Duplicate files exist in directory:", 
+                     file_list[idx])
     # Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+    return results_dictionary
